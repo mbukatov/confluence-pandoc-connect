a10c56ba16d5a4bc6194fbed0c39dce257504f04
Avi Knoll <aknoll@atlassian.com>
2015-06-16 12:19:02 +1000

    CPC-9 adding lifecycle routes
    still need to add the AtlassianConnect snaplet to Application

--- src/Site.hs
+++ src/Site.hs
@@ -1,7 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-------------------------------------------------------------------------------
 -- | This module is where all the routes and handlers are defined for your
 -- site. The 'app' function is the initializer that combines everything
 -- together and is exported by this module.
@@ -9,66 +8,32 @@ module Site
   ( app
   ) where
 
-------------------------------------------------------------------------------
-import Prelude hiding (readFile)
+import           Application
 import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.IO.Class
-import           Data.ByteString (ByteString, readFile)
-import           Data.ByteString.Char8 (pack, unpack)
-import qualified Data.ByteString.Lazy as LBS
+import           Data.ByteString                             (ByteString,
+                                                              readFile)
+import           Data.ByteString.Char8                       (pack, unpack)
+import qualified Data.ByteString.Lazy                        as LBS
 import           Data.Maybe
 import           Data.Monoid
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as E
+import qualified Data.Text                                   as T
+import qualified Data.Text.Encoding                          as E
+import           Heist
+import qualified Heist.Interpreted                           as I
+import           LifecycleHandlers
+import           Prelude                                     hiding (readFile)
 import           Snap.Core
 import           Snap.Snaplet
 import           Snap.Snaplet.Auth
 import           Snap.Snaplet.Auth.Backends.JsonFile
 import           Snap.Snaplet.Heist
-import           Snap.Snaplet.Session.Backends.CookieSession
 import           Snap.Snaplet.PostgresqlSimple
+import           Snap.Snaplet.Session.Backends.CookieSession
 import           Snap.Util.FileServe
 import           Snap.Util.FileUploads
 import           Text.Pandoc
-import           Heist
-import qualified Heist.Interpreted as I
-------------------------------------------------------------------------------
-import           Application
-
-
-------------------------------------------------------------------------------
--- | Render login form
-handleLogin :: Maybe T.Text -> Handler App (AuthManager App) ()
-handleLogin authError = heistLocal (I.bindSplices errs) $ render "login"
-  where
-    errs = maybe mempty splice authError
-    splice err = "loginError" ## I.textSplice err
-
-
-------------------------------------------------------------------------------
--- | Handle login submit
-handleLoginSubmit :: Handler App (AuthManager App) ()
-handleLoginSubmit =
-    loginUser "login" "password" Nothing
-              (\_ -> handleLogin err) (redirect "/")
-  where
-    err = Just "Unknown user or password"
-
-
-------------------------------------------------------------------------------
--- | Logs out and redirects the user to the site index.
-handleLogout :: Handler App (AuthManager App) ()
-handleLogout = logout >> redirect "/"
-
-
-------------------------------------------------------------------------------
--- | Handle new user form submit
-handleNewUser :: Handler App (AuthManager App) ()
-handleNewUser = method GET handleForm <|> method POST handleFormSubmit
-  where
-    handleForm = render "new_user"
-    handleFormSubmit = registerUser "login" "password" >> redirect "/"
 
 heartbeatRequest :: Handler App App ()
 heartbeatRequest = putResponse $ setResponseCode 200 emptyResponse
@@ -174,21 +139,16 @@ writeConfluenceStorageFormat pandoc = do
   heistLocal (I.bindStrings splices) $ render "storage_formatted_response"
   return ()
 
-------------------------------------------------------------------------------
 -- | The application's routes.
-routes :: [(ByteString, Handler App App ())]
-routes = [ ("/login",    with auth handleLoginSubmit)
-         , ("/logout",   with auth handleLogout)
-         , ("/new_user", with auth handleNewUser)
-         , ("rest/heartbeat", heartbeatRequest)
-         , ("",          serveDirectory "static")
-         , ("/atlassian-connect.json", serveDescriptor)
-         , ("/create", handleCreateRequest)
-         , ("/all.js", serveFile "resources/all.js")
-         ]
+routes, applicationRoutes :: [(ByteString, Handler App App ())]
+routes = applicationRoutes ++ lifecycleRoutes
+applicationRoutes =
+  [ ("rest/heartbeat", heartbeatRequest)
+  , ("/atlassian-connect.json", serveDescriptor)
+  , ("/create", handleCreateRequest)
+  , ("/all.js", serveFile "resources/all.js")
+  ]
 
-
-------------------------------------------------------------------------------
 -- | The application initializer.
 app :: SnapletInit App App
 app = makeSnaplet "app" "An snaplet example application." Nothing $ do
