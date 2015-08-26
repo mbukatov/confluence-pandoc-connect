e8d0531026151569725da140fcaf031e1eab5bd1
Brian McKenna <brian@brianmckenna.org>
2015-08-26 10:48:42 +1000

    Update run-confluence.sh from Gardener Tutorial

--- run-confluence.sh
+++ run-confluence.sh
@@ -2,4 +2,4 @@
 
 # See https://developer.atlassian.com/static/connect/docs/latest/guides/confluence-gardener-tutorial.html
 
-atlas-run-standalone --product confluence --version 5.9.0-OD-55-030 --bundled-plugins com.atlassian.bundles:json-schema-validator-atlassian-bundle:1.0.4,com.atlassian.webhooks:atlassian-webhooks-plugin:1.0.6,com.atlassian.jwt:jwt-plugin:1.2.2,com.atlassian.upm:atlassian-universal-plugin-manager-plugin:2.19.0.3-D20150522T180613,com.atlassian.plugins:atlassian-connect-plugin:1.1.33 --jvmargs -Datlassian.upm.on.demand=true
+atlas-run-standalone --product confluence --version 5.9.1-OD-65-062 --bundled-plugins com.atlassian.bundles:json-schema-validator-atlassian-bundle:1.0.4,com.atlassian.webhooks:atlassian-webhooks-plugin:1.0.6,com.atlassian.jwt:jwt-plugin:1.2.2,com.atlassian.upm:atlassian-universal-plugin-manager-plugin:2.19.1.2-D20150723T232127,com.atlassian.plugins:atlassian-connect-plugin:1.1.46 --jvmargs -Datlassian.upm.on.demand=true
