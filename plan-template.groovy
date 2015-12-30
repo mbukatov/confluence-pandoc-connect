66ce569d7a4c1a64acd38804791be02c36202496
Avi Knoll <aknoll@atlassian.com>
2015-12-30 13:40:28 +1100

    Use ADD not COPY for static files in run image

--- plan-template.groovy
+++ plan-template.groovy
@@ -89,7 +89,7 @@ EXPOSE 8080
 RUN apt-get update && apt-get install -y openjdk-7-jre-headless
 
 # Install static files
-COPY snaplets resources migrations static /service/
+ADD snaplets resources migrations static /service/
 
 # Copy and run binary
 ADD ./confluence-pandoc-connect /usr/bin/confluence-pandoc-connect
