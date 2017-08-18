4f9cd0e50ade8e098eacf22f850f6a030f951e04
Avi Knoll <aknoll@atlassian.com>
2017-08-18 13:52:06 +1000

    CPC-65 flyway migration stage to rebuild table safely
    Previous migration broke the serial primary key index.

---
+++ migrations/sql/V3__rebuild_tenant_index.sql
@@ -0,0 +1,19 @@
+ALTER TABLE tenant RENAME TO tenant_v2;
+
+CREATE TABLE tenant (
+   id  SERIAL PRIMARY KEY
+,  key VARCHAR(255) unique not null
+,  publicKey TEXT not null
+,  oauthClientId TEXT null
+,  sharedSecret VARCHAR(512) null
+,  baseUrl VARCHAR(512) unique not null
+,  productType VARCHAR(50) not null
+,  sleep_date TIMESTAMP WITH TIME ZONE
+);
+
+INSERT INTO tenant (key, publicKey, oauthClientId, sharedSecret, baseUrl, productType, sleep_date)
+SELECT key, publicKey, oauthClientId, sharedSecret, baseUrl, productType, sleep_date FROM tenant_old;
+
+DROP INDEX tenant_idx;
+
+CREATE INDEX tenant_idx ON tenant (sharedSecret);
